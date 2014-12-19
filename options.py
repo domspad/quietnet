786646e7d37277f7c103405da9ff4456fe426f7f
domspad <domspad@umich.edu>
2014-12-18 23:18:19 -0800

    just tinkering with options

--- options.py
+++ options.py
@@ -1,7 +1,14 @@
-rate = 44100
-freq = 19100# chosen because it is outside most people's hearing and worked for my microphone and speakers
-channels = 1
-frame_length = 3
-chunk = 256
+rate = 44100  #pitch related (diff with freq?)
+freq = 500			# chosen because it is outside most people's hearing and worked for my microphone and speakers
+channels = 1   #pitch and speed related - only can be 1 or 2
+frame_length = 10 #size of beeps
+chunk = 256		#size of beeps
 datasize = chunk * frame_length
 sigil = "00"
+
+"""
+For making sense of psk:
+freq 500
+frame_length = 20
+
+"""
